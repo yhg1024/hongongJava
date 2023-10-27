b3d71c75924510f1c7bbd06c874f1b96f263695b
dgr1024kr <dgr1024kr@naver.com>
2023-10-27 09:38:34 +0900

    test

---
+++ src/ch01/sec02/exam01/VariableInitializationExample.java
@@ -0,0 +1,18 @@
+package ch01.sec02.exam01;
+
+public class VariableInitializationExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 변수 value 선언
+		int value;
+		
+		// 변수 value의 값을 읽고 10을 더하는 산술연산을 수행
+		// 연산의 결과값을 변수 result의 초기값으로 저장
+		int result = value + 10;
+		
+		// 변수 result 값을 읽고 콘솔에 출력
+		System.out.println(result);
+	}
+
+}
